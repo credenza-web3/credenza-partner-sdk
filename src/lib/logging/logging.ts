316639ba6b1b8bb1c04734597a2a183bca44e1d7
dfokin <dmitry.fokin@macrodemic.com>
2024-07-30 12:11:00 +0300

    fix

--- src/lib/logging/logging.ts
+++ src/lib/logging/logging.ts
@@ -2,7 +2,7 @@ let debugEnabled = false
 
 export function log(methodName: string, ...args: unknown[]) {
   if (!debugEnabled) return
-  console.log('[CREDENZA_PARTNER_SDK]:', `(${methodName})`, ...args)
+  console.log(`[CREDENZA_PARTNER_SDK#${methodName}]:`, ...args)
 }
 
 export function enableSdkDebug(enableDebug: boolean) {
