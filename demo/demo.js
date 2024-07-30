d73b7be344bbacf5977de5d634ac3753981060c3
dfokin <dmitry.fokin@macrodemic.com>
2024-07-30 12:40:49 +0300

    fixes

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,6 @@
 const { useAuth, useEnv, debug } = require('@credenza3/partner-sdk')
 const { requestPassportIdSignature } = require('@credenza3/partner-sdk/common')
+const { getSuiAddress } = require('@credenza3/partner-sdk/sui')
 
 async function run() {
   debug(true)
@@ -11,8 +12,11 @@ async function run() {
   
   // const address = await getEvmAddress('6581c0ce6a82d99c2356db66')
   // console.log(address)
-  const sig = await requestPassportIdSignature('6581c0ce6a82d99c2356db66', 'sign this message')
+  const sig = await requestPassportIdSignature('6581c0ce6a82d99c2356db66', 'test')
   console.log(sig)
+
+  const addr = await getSuiAddress('6581c0ce6a82d99c2356db66')
+  console.log(addr)
   process.exit()
 }
 
--- src/sui/index.ts
+++ src/sui/index.ts
@@ -1,2 +1,3 @@
 export * from './sui'
+export * from './account'
 export * as zk from './zk'
