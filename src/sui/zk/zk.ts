50f96565d7c610adcb7b0fc3db0ec66f385232a5
dfokin <dmitry.fokin@macrodemic.com>
2024-07-30 09:40:14 +0300

    fix

--- src/sui/zk/zk.ts
+++ src/sui/zk/zk.ts
@@ -1,12 +1,12 @@
 import { getBasicToken } from '../../lib/credentials'
 import { getSuiApiUrl } from '../sui'
 
-export async function getSuiZkAddress(sub: string): Promise<{address: string}> {
+export async function getSuiZkAddress(sub: string): Promise<{ address: string }> {
   const response = await fetch(`${getSuiApiUrl()}/accounts/zk/${sub}/address`, {
     headers: {
       Authorization: getBasicToken(),
       'Content-Type': 'application/json',
-    }
+    },
   })
   const json = await response.json()
   return json
