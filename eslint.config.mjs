d868c3624c9bde51f60e4348d61953be6ca1bb5b
dfokin <dmitry.fokin@macrodemic.com>
2024-09-12 12:22:13 +0300

    eslint upd

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/.eslintrc.js", "demo/*"],
+}, ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslintEslintPlugin,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+            tsconfigRootDir: "/Users/dmytrofokin/credenza/credenza-partner-sdk",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-floating-promises": "error",
+        eqeqeq: ["error", "always"],
+        semi: ["error", "never"],
+
+        "max-lines-per-function": ["error", {
+            max: 70,
+            skipBlankLines: true,
+            skipComments: true,
+        }],
+
+        complexity: ["error", 10],
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            ignoreRestSiblings: true,
+            varsIgnorePattern: "^_",
+        }],
+    },
+}];
