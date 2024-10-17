21be7808c47c4ddf70aefa831fb6ab9a849f49cc
dfokin <dmitry.fokin@macrodemic.com>
2024-10-17 14:36:31 +0300

    fix

--- jest.setup.spec.ts
+++ jest.setup.spec.ts
@@ -1,13 +1,12 @@
 import { config as dotenvConfig } from 'dotenv'
 
 import { useAuth, useEnv, useDebug } from './src/credenza-partner-sdk'
-import type { TSdkEnv } from './src/lib/sdk-env'
 
 dotenvConfig()
 
 beforeEach(() => {
   useDebug(false)
-  useEnv(process.env.CLIENT_ENV as TSdkEnv)
+  useEnv('local')
   useAuth({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
