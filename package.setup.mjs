ade400cc1dc1a141d8151ebcd43f5b19f312e51d
dfokin <dmitry.fokin@macrodemic.com>
2024-10-18 11:21:28 +0300

    fix

--- package.setup.mjs
+++ package.setup.mjs
@@ -3,30 +3,30 @@ import * as path from 'path'
 
 const DIST_DIR = './dist';
 
-let exportsUpd = {};
+const exportsUpd = {};
 function generateExports(dir, exportPath) {
   if (/^lib/.test(exportPath)) return
 
   fs.readdirSync(dir, { withFileTypes: true }).forEach(dirent => {
     const fullPath = path.join(dir, dirent.name);
     const relativePath = path.relative('.', fullPath);
-    const exportKey = `./${exportPath}`;
+    const exportKey = exportPath ? `./${exportPath}` : '.';
 
     if (dirent.isDirectory()) {
       generateExports(fullPath, path.join(exportPath, dirent.name));
     } else if (dirent.isFile() && dirent.name === 'index.js') {
       exportsUpd[exportKey] = {
         "import": `./${relativePath}`,
-        "require": `./${relativePath}`
+        "require": `./${relativePath}`,
+        "types": `./${relativePath.replace('.js', '.d.ts')}`,
       };
     }
   });
 }
 generateExports(DIST_DIR, '');
 
-//console.log(exportsUpd)
-
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const packageJsonPath = './package.json'
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 packageJson.exports = exportsUpd;
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8');
 
