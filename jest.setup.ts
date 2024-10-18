164e058903510ec87f6cac5060efce5e87421c3b
dfokin <dmitry.fokin@macrodemic.com>
2024-10-18 11:33:34 +0300

    fix

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,10 +5,11 @@ import { useAuth, useEnv, useDebug } from './src/credenza-partner-sdk'
 dotenvConfig()
 
 beforeAll(() => {
+  const clientId = process.env.CLIENT_ID as string
+  const clientSecret = process.env.CLIENT_SECRET as string
+  if (!clientId || !clientSecret) throw new Error('Missing credentials')
+
   useDebug(false)
   useEnv('local')
-  useAuth({
-    clientId: process.env.CLIENT_ID as string,
-    clientSecret: process.env.CLIENT_SECRET as string,
-  })
+  useAuth({ clientId, clientSecret })
 })
