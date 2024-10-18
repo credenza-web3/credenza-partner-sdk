4c8e366dd57a3561c0114a3e82eb97c25bbe63e1
dfokin <dmitry.fokin@macrodemic.com>
2024-10-18 15:56:23 +0300

    a7

--- package.setup.mjs
+++ package.setup.mjs
@@ -27,16 +27,38 @@ function generateExports(dir, exportPath) {
 }
 generateExports(DIST_DIR, '');
 
-const sortedExports = Object.keys(exportsUpd)
-  .sort((a, b) => b.split('/').length - a.split('/').length) // Sort by depth, deeper paths first
-  .reduce((acc, key) => {
-    acc[key] = exportsUpd[key];
-    return acc;
-  }, {});
+// const sortedExports = Object.keys(exportsUpd)
+//   .sort((a, b) => b.split('/').length - a.split('/').length) // Sort by depth, deeper paths first
+//   .reduce((acc, key) => {
+//     acc[key] = exportsUpd[key];
+//     return acc;
+//   }, {});
+
+const sortedExports = {
+  ".": {
+    "import": "./build/esm/index.js",
+    "require": "./build/cjs/index.js"
+  },
+  // This is necessary to require/import `your-library/package.json`
+  "./package.json": "./package.json",
+  // or everything in the source root:
+  "./*": {
+    "import": "./build/esm/*.js",
+    "require": "./build/cjs/*.js"
+  }
+}
 
 const packageJsonPath = './package.json'
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 packageJson.exports = sortedExports;
+packageJson.types = "dist/esm/index.d.ts",
+packageJson.typesVersions = {
+		"*": {
+			"dist/esm/index.d.ts": ["dist/esm/index.d.ts"],
+			"dist/cjs/index.d.ts": ["dist/esm/index.d.ts"],
+			"*": ["dist/esm/*"]
+		}
+	}
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8');
 
 console.log('Exports field updated in package.json');
