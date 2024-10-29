7c79104d3cb3f2a8bd363ef828028fef898c22db
dfokin <dmitry.fokin@macrodemic.com>
2024-10-29 15:50:05 +0200

    multiple socket handeling

--- src/lib/ws/ws.ts
+++ src/lib/ws/ws.ts
@@ -1,19 +1,22 @@
 import { getCredentials, TCredentials } from '@/lib/credentials'
 import { io, Socket } from 'socket.io-client'
 
-let socketPromise: Promise<{ socket: Socket; auth: TCredentials }> | null = null
+const socketPromises = new Map<string, Promise<{ socket: Socket; auth: TCredentials }>>()
 
-export async function disconnectWs() {
-  if (!socketPromise) return
-  const { socket } = await socketPromise
-  socket?.disconnect()
-  socketPromise = null
+export async function disconnectWs(url: string = null) {
+  const socketUrls = url ? [url] : Array.from(socketPromises.keys())
+  for (const url of socketUrls) {
+    if (!socketPromises.has(url)) continue
+    const { socket } = await socketPromises.get(url)
+    socket?.disconnect()
+    socketPromises.delete(url)
+  }
 }
 export async function connectToWs(url: string): Promise<Socket> {
   const credentials = getCredentials()
 
-  if (!socketPromise) {
-    socketPromise = new Promise((resolve, reject) => {
+  if (!socketPromises.has(url)) {
+    const promise = new Promise<{ socket: Socket; auth: TCredentials }>((resolve, reject) => {
       const socket = io(url, {
         auth: {
           client_id: credentials.clientId,
@@ -24,11 +27,12 @@ export async function connectToWs(url: string): Promise<Socket> {
       socket.once('connect', () => resolve({ socket, auth: credentials }))
       setTimeout(() => reject(new Error('Cannot connect to websocket server')), 5000)
     })
+    socketPromises.set(url, promise)
   }
 
-  const { socket, auth } = await socketPromise
+  const { socket, auth } = await socketPromises.get(url)
   if (!socket?.connected || auth.clientId !== credentials.clientId || auth.clientSecret !== credentials.clientSecret) {
-    await disconnectWs()
+    await disconnectWs(url)
     return connectToWs(url)
   }
 
