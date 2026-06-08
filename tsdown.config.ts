87b66c5aacb24d646aee7ced81902686dc232c1a
dfokin <dmitry.fokin@macrodemic.com>
2026-06-08 14:53:23 +0300

    removed common js build, Seems unnecessary.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: 'esm',
   sourcemap: true,
   dts: true,
   clean: true,
   target: 'es6',
-  outExtensions: () => ({ dts: '.d.ts' }),
 })
